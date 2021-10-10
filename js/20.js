4783467701d72df3eb7f6631e6b32166bb91a822
Eric <ekameyshaaa@gmail.com>
2021-10-10 12:34:06 +0300

    function swift

--- js/20.js
+++ js/20.js
@@ -1,8 +1,8 @@
 // Setup
 var myArray = [
   ["John", 23],
-  ["cat", 2]
+  ["dog", 3]
 ];
 
 // Only change code below this line
-myArray.push(["dog", 3])
+var removedFromMyArray = myArray.shift();
