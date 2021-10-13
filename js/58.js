d77d759e0423ecde4125e90e608efcd03fbbadfa
Eric <ekameyshaaa@gmail.com>
2021-10-13 13:51:38 +0300

    управление сложными объектами

---
+++ js/58.js
@@ -0,0 +1,18 @@
+var myMusic = [{
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    'artist': 'Billy Jine',
+    'title': 'drinking men',
+    'release_year': 2002,
+    'formats': ['cd', 'lp']
+  }
+];
